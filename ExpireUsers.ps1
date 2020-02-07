5f2fb191f3a12f0867700c1bfcbe668ce3a582fa
MassoniMacaroni <60683297+MassoniMacaroni@users.noreply.github.com>
2020-02-07 15:52:34 +1100

    Add files via upload

---
+++ ExpireUsers.ps1
@@ -0,0 +1,14 @@
+﻿$UserList = import-csv \\8cc9224ppb\C$\Temp\Scripts\ExpireUserList.csv 
+
+ForEach ($Username in $UserList) {
+    $User = Get-ADUser $Username -properties pwdLastSet
+    Set-ADUser -Identity $User -PasswordNeverExpires:$False
+    
+    Write-Host "Processing "$User -ForegroundColor Yellow
+	$User.pwdLastSet = 0
+	Set-ADUser -Instance $User -Server HCC-S-SRAD05
+	$User.pwdLastSet = -1
+	
+    Write-Host "Processed "$User -ForegroundColor Green
+    #Set-ADUser -ChangePasswordAtLogon:$true
+    }
