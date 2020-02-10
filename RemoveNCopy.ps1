51723cda6bc1d14f4c5bc3f37ea6d10c40d0ba5d
MassoniMacaroni <60683297+MassoniMacaroni@users.noreply.github.com>
2020-02-10 15:08:22 +1100

    Add files via upload

---
+++ RemoveNCopy.ps1
@@ -0,0 +1,8 @@
+﻿$HostName = get-content 'C:\Temp\Scripts\CScomputerlist.csv'
+
+
+foreach ($PC in $HostName) {
+    $RemotePath = "\\$PC\C$\Users\Public\Desktop\Cisco Finesse - Backup.url"
+    remove-item -path $RemotePath -force -recurse
+    copy-item "\\hcc-dom\files\Common\Rigo\Cisco Finesse.url" -Destination "\\$PC\C$\Users\Public\Desktop"
+    }
