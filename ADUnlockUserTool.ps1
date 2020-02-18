c2424bde8b5272b79c113504e181f96e7523c257
MassoniMacaroni <60683297+MassoniMacaroni@users.noreply.github.com>
2020-02-18 11:11:15 +1100

    Update ADUnlockUserTool.ps1

--- ADUnlockUserTool.ps1
+++ ADUnlockUserTool.ps1
@@ -10,14 +10,17 @@ clear-host
 foreach ($account in $AccountArray) {
 
     #Prints Index and relative hostname on screen
-    write-host $Index - $Account.Name
+    write-host "`n" $Index - $Account.Name
     #Increments Index
     $Index ++
 }
 
 #Input for the index reference value used to determine which account to unlock
-$IndexRef = Read-host -Prompt "Select Account No. to unlock"
+$IndexRef = Read-host -Prompt "`nSelect Account No. to unlock"
 #Unlocks the specified AD account from Index Ref value
 Unlock-ADAccount -identity $AccountArray[($IndexRef)]
 
+Write-host "`n" $AccountArray[($IndexRef)].name "has been unlocked. `n" 
 
+Write-Host -NoNewLine 'Press any key to continue...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
