4b386e6993e0c8604631fa9c750445b52a0c3209
MassoniMacaroni <60683297+MassoniMacaroni@users.noreply.github.com>
2020-02-18 11:50:22 +1100

    Update ADUnlockUserTool.ps1

--- ADUnlockUserTool.ps1
+++ ADUnlockUserTool.ps1
@@ -8,19 +8,28 @@ clear-host
 
 #Loops through array
 foreach ($account in $AccountArray) {
-
     #Prints Index and relative hostname on screen
     write-host "`n" $Index - $Account.Name
     #Increments Index
     $Index ++
 }
 
-#Input for the index reference value used to determine which account to unlock
-$IndexRef = Read-host -Prompt "`nSelect Account No. to unlock"
-#Unlocks the specified AD account from Index Ref value
-Unlock-ADAccount -identity $AccountArray[($IndexRef)]
 
-Write-host "`n" $AccountArray[($IndexRef)].name "has been unlocked. `n" 
 
-Write-Host -NoNewLine 'Press any key to continue...'
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+if($AccountArray.Length -lt 1) {
+    #Print message if no  accounts are locked
+    Write-Host "No accounts available to be unlocked"
+
+}else{
+    #Input for the index reference value used to determine which account to unlock
+    $IndexRef = Read-host -Prompt "`nSelect Account No. to unlock" 
+    #Unlocks the specified AD account from Index Ref value
+    Unlock-ADAccount -identity $AccountArray[($IndexRef)]
+    #Print account unlocked
+    Write-host "`n" $AccountArray[($IndexRef)].name "has been unlocked. `n"
+
+}
+
+#Press any key to continue
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
